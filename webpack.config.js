0357c0be11020984f8c812510dfb9c8a368b6826
Drake Wilcox <drakewilcox@gmail.com>
2020-02-14 10:13:53 -0800

    add rest of initial files including .env and API key.

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'triangle-tracker',
+      title: 'doctor',
       template: './src/index.html',
       inject: 'body'
     })
