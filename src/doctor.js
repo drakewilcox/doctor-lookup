6b38945bc91fb7aa7611ee70a3074151f8c25139
Drake Wilcox <drakewilcox@gmail.com>
2020-02-14 14:36:29 -0800

    add logic to user interface to gather user input by using jQuery.

--- src/doctor.js
+++ src/doctor.js
@@ -4,9 +4,6 @@ export class DoctorSearch {
       let response = await fetch( 'https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${special}&location=${state}-${city}&gender=${gender}&skip=0&limit=10&user_key=${process.env.API_KEY}');
 
 
-      'https://api.betterdoctor.com/2016-03-01/doctors?first_name=xavier&last_name=preciado&query=medical&location=or-portland&gender=male&skip=0&limit=10&user_key=${process.env.API_KEY}'
-
-
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifedResponse = await response.json();
--- src/main.js
+++ src/main.js
@@ -7,14 +7,27 @@ import { DoctorSearch } from './../src/doctor.js';
 
 $(document).ready(function() {
 
-  (async () => {
-    let doctor = new DoctorSearch();
-    const response = await doctor.getDoctor();
-    getElements(response);
-  })();
-
-  function getElements(response) {
-    if (response) {}
-  }
-
+  $('#searchDoctor').click(function() {
+    const firstName = $('#firstName').val();
+    const lastName = $('#lastName').val();
+    const special = $('#special').val();
+    const state = $('#state').val();
+    const gender = $('#gender').val();
+    $('#firstName').val("");
+    $('#lastName').val("");
+    $('#special').val("");
+    $('#state').val("");
+    $('#gender').val("");
+    
+    (async () => {
+      let doctor = new DoctorSearch();
+      const response = await doctor.getDoctor();
+      getElements(response);
+    })();
+    
+    function getElements(response) {
+      if (response) {}
+    }
+    
+  });
 });
