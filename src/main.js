a530610867ff02782db192ea866f4caa31202c40
Drake Wilcox <drakewilcox@gmail.com>
2020-02-14 16:29:28 -0800

    finally got form inputs to send needed information.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-$
+$;
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
@@ -7,17 +7,27 @@ import { DoctorSearch } from './../src/doctor.js';
 
 $(document).ready(function() {
 
-  $('#searchDoctor').click(function() {
-    const firstName = $('#firstName').val();
+  $('#searchDoctor').submit(function(event) {
+    event.preventDefault(); 
+
+    const firstName = $("#firstName").val();
     const lastName = $('#lastName').val();
-    const special = $('#special').val();
+    // const special = $('#special').val();
     const state = $('#state').val();
+    const city = $('#city').val();
     const gender = $('#gender').val();
     $('#firstName').val("");
     $('#lastName').val("");
     $('#special').val("");
     $('#state').val("");
+    $("#city").val();
     $('#gender').val("");
+    console.log(firstName);
+    console.log(lastName);
+    // console.log(special);
+    console.log(state);
+    console.log(city);
+    console.log(gender);
     
     (async () => {
       let doctor = new DoctorSearch();
