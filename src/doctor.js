97a538692f4a8f01a797fca78c3d60ecc317cf3f
Drake Wilcox <drakewilcox@gmail.com>
2020-02-16 18:39:42 -0800

    cleaned up display of results.

--- src/doctor.js
+++ src/doctor.js
@@ -9,13 +9,13 @@ export class DoctorSearch {
       console.log(gender);
      
       let response = await fetch( `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${special}&location=${state}-${city}&skip=0&limit=10&user_key=${process.env.API_KEY}`);
-      console.log(response);
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
       } else {
         jsonifiedResponse = false;
       }
+      console.log(jsonifiedResponse)
       return jsonifiedResponse;
     } catch(error) {
       return false;
--- src/main.js
+++ src/main.js
@@ -9,42 +9,39 @@ $(document).ready(function() {
 
   $('#searchDoctor').submit(function(event) {
     event.preventDefault(); 
-
+    
+    let doctor = new DoctorSearch();
     const firstName = $("#firstName").val();
     const lastName = $('#lastName').val();
     const special = $('#special').val();
     const state = $('#state').val();
-    const city = $('#city').val();
-    console.log(state);
-    console.log(city)
-    $('#firstName').val("");
-    $('#lastName').val("");
-    $('#special').val("");
-    $('#state').val("");
-    $("#city").val();
-
-
- 
-    
+    const city = $('#city').val().toLowerCase();
+    // console.log(state);
+    // console.log(city)
+    // $('#firstName').val("");
+    // $('#lastName').val("");
+    // $('#special').val("");
+    // $('#state').val("");
+    // $("#city").val();
+    if (city === "" || state === "Choose...") {
+      $("#alert").show();
+      } else {
+      $("#alert").hide();
+    }
     (async () => {
-      let doctor = new DoctorSearch();
       let response = await doctor.getDoctor(firstName, lastName, special, state, city);
       getElements(response);
       function getElements(response) {
         if (response) {
           response.data.forEach(about => {
+            console.log(about.practices[0])
             about.practices.forEach(location => {
-              console.log(about.profile.first_name);
-              console.log(location.visit_address)
+              console.log(about.practices)
               $("#showDoctors").append("<br>" + "Doctor Name: " + about.profile.first_name + " " + about.profile.last_name + "<br>" + "Address: " + location.visit_address.street + "<br>" + location.visit_address.city + "," + location.visit_address.state + " " + location.visit_address.zip + "<br>" + "Phone Number: " + location.phones[0].number + "<br>")
-  
             });
           });
         }
       }
     })();
-    
-      
-    
   });
 });
